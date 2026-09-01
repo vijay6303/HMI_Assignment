bd47dde509ef2b837fbb1e868163e0fc720314b2
Vijay Kumar <rachakondavijay18@gmail.com>
2026-09-01 09:34:22 +0530

    Update CORS with exact Vercel URL

--- backend/src/main/java/com/primeform/vmc/config/CorsConfig.java
+++ backend/src/main/java/com/primeform/vmc/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React frontend URL
+                .allowedOrigins("https://frontend-seven-liard-8fgrtwp5zy.vercel.app/","http://localhost:3000") // React frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
